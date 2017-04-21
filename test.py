acd967a9519963c2158dca1de1828f01bb15667e
createnewli <liyang19911229@163.com>
2017-04-21 14:08:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 2017-04-20 18:20:29
-# @Author  : Your Name (you@example.org)
-# @Link    : http://example.org
-# @Version : $Id$
 
 
 import sys
@@ -13,5 +9,5 @@ from MagicViewer import ImageViewer
 
 app = QApplication(sys.argv)
 viewer = ImageViewer()
-viewer.open("C:\\Users\\李杨\Desktop\\新建脑图.png")
+viewer.open("C:\\Users\\youth_lee\Desktop\\newimage.png")
 sys.exit(app.exec_())
